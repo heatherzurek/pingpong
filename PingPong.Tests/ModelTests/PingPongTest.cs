431364d6a43db0584f55c48d932fe4bdf0d3140a
Heather Zurek and Reese Lee <reese@>
2019-04-23 10:36:29 -0700

    3 passing tests

--- PingPong.Tests/ModelTests/PingPongTest.cs
+++ PingPong.Tests/ModelTests/PingPongTest.cs
@@ -10,7 +10,19 @@ namespace PingPongGame.Tests
       public void IsPingPong_NumberDivisibleByThree_True()
       {
         PingPong testPingPong = new PingPong();
-        Assert.AreEqual(true, testPingPong.IsPingPong(31));
+        Assert.AreEqual(true, testPingPong.IsPing(33));
+      }
+      [TestMethod]
+      public void IsPingPong_NumberDivisibleByFive_True()
+      {
+        PingPong testPingPong = new PingPong();
+        Assert.AreEqual(true, testPingPong.IsPong(50));
+      }
+      [TestMethod]
+      public void IsPingPong_NumberDivisibleByThreeAndFive_True()
+      {
+        PingPong testPingPong = new PingPong();
+        Assert.AreEqual(true, testPingPong.IsPingPong(15));
       }
     }
 }
--- PingPong/Models/ping-pong.cs
+++ PingPong/Models/ping-pong.cs
@@ -7,9 +7,17 @@ namespace PingPongGame
     // static void Main()
     // {
       // int userInput = int.Parse(Console.ReadLine());
-      public bool IsPingPong(int number)
+      public bool IsPing(int number)
       {
         return number % 3 == 0;
+      }
+      public bool IsPong(int number)
+      {
+        return number % 5 == 0;
+      }
+      public bool IsPingPong(int number)
+      {
+        return number % 3 == 0 && number % 5 == 0;
       }
       // for (i = 0; i <= userInput; i++) {
       //   if(i % 3 == 0 && i % 5 == 0)
